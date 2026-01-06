a44ab8723fcf6be749b24f1ad6fc9aecb6f7f04f
John Goh <johngohrw@gmail.com>
2026-01-06 23:39:22 +0800

    huge refactor

--- astro-instance/src/components/RenderBlocks/types.ts
+++
@@ -1,9 +0,0 @@
-import type { Page } from "payload-instance";
-
-export type BlockType = NonNullable<Page["contentBlocks"]>[number];
-export type BlockTypeEnum = BlockType["blockType"];
-export type BlockTypeOf<T extends BlockTypeEnum> = Extract<
-  BlockType,
-  { blockType: T }
->;
-export type BlockComponent = (_props: { block: BlockType }) => any;
